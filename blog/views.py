81041241f5736c4c84570a9af98675b3ee5568dd
unknown <BORNIA ALEX@ISIS.Luzzatto1>
2025-05-19 11:39:16 +0200

    cambiato titolo della home

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        'messaggio': 'Benvenuto nella homepage!'
+        'messaggio': 'Benvenuto nella home!'
     }
 
     return render(request, 'home.html', context)
